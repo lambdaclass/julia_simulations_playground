0e09f53968981d1a4ea0023807273137caddb269
Iván Litteri <ilitteri@fi.uba.ar>
2022-06-07 17:15:18 -0300

    Implement served ladder estimation

--- generala_simulations.jl
+++ generala_simulations.jl
@@ -33,6 +33,17 @@ function served_poker_probability_estimation(num_throws=1_000_000)
     return sum(throws)/num_throws
 end
 
+function served_ladder_probability_estimation(num_throws=1_000_000)
+    throws = zeros(num_throws)
+    for i in 1:num_throws
+        throw = throw_dice(5)
+        sort!(throw)
+        throws[i] = throw == [1, 2, 3, 4, 5] || throw == [2, 3, 4, 5, 6]
+    end
+    return sum(throws)/num_throws
+end
+
 println("The probability of a served generala is: $(served_generala_probability_estimation())")
 println("The probability of a served full is: $(served_full_probability_estimation())")
 println("The probability of a served poker is: $(served_poker_probability_estimation())")
+println("The probability of a served ladder is: $(served_ladder_probability_estimation())")
