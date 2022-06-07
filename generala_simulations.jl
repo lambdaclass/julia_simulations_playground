fd9c24cea002e0d60bcd00ffd85263fc2cd566bc
Iván Litteri <ilitteri@fi.uba.ar>
2022-06-07 17:14:20 -0300

    Implement served full estimation

--- generala_simulations.jl
+++ generala_simulations.jl
@@ -15,4 +15,14 @@ function served_generala_probability_estimation(num_throws=1_000_000)
     return sum(throws) / num_throws
 end
 
+function served_full_probability_estimation(num_throws=1_000_000)
+    throws = zeros(num_throws)
+    for i in 1:num_throws
+        throw = throw_dice(5)
+        throws[i] = length(unique(throw)) == 2 && 2 in values(countmap(throw))
+    end
+    return sum(throws)/num_throws
+end
+
 println("The probability of a served generala is: $(served_generala_probability_estimation())")
+println("The probability of a served full is: $(served_full_probability_estimation())")
