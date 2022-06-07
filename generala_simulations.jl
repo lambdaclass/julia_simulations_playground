c6760b81c87f2a4b632a139af00f1b3b41b7b595
Iván Litteri <ilitteri@fi.uba.ar>
2022-06-07 17:13:54 -0300

    Implement served generala estimation

--- generala_simulations.jl
+++ generala_simulations.jl
@@ -5,3 +5,14 @@ SIX_SIDES_DICE = 1:6
 function throw_dice(dice_count)
     return rand(SIX_SIDES_DICE, dice_count)
 end
+
+function served_generala_probability_estimation(num_throws=1_000_000)
+    throws = zeros(num_throws)
+    for i in 1:num_throws
+        throw = throw_dice(5)
+        throws[i] = all(y -> y == throw[1], throw)
+    end
+    return sum(throws) / num_throws
+end
+
+println("The probability of a served generala is: $(served_generala_probability_estimation())")
