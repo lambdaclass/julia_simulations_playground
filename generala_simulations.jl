200bc3ec89711515bc390f92ac489126cf9c6a66
Iván Litteri <ilitteri@fi.uba.ar>
2022-06-07 17:14:49 -0300

    Implement served poker estimation

--- generala_simulations.jl
+++ generala_simulations.jl
@@ -24,5 +24,15 @@ function served_full_probability_estimation(num_throws=1_000_000)
     return sum(throws)/num_throws
 end
 
+function served_poker_probability_estimation(num_throws=1_000_000)
+    throws = zeros(num_throws)
+    for i in 1:num_throws
+        throw = throw_dice(5)
+        throws[i] = length(unique(throw)) == 2 && 4 in values(countmap(throw))
+    end
+    return sum(throws)/num_throws
+end
+
 println("The probability of a served generala is: $(served_generala_probability_estimation())")
 println("The probability of a served full is: $(served_full_probability_estimation())")
+println("The probability of a served poker is: $(served_poker_probability_estimation())")
