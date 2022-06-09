9e5d0f173a02b99d58454b70bfa0db9da04569ee
Iván Litteri <ilitteri@fi.uba.ar>
2022-06-09 18:17:27 -0300

    Add simulation setup

--- leader_election_simulation/leader_election_simulation.jl
+++ leader_election_simulation/leader_election_simulation.jl
@@ -81,8 +81,42 @@ function simulate_leader_election_n_rounds(num_rounds, validators_pool)
     return Dict(string(k)=>v  for (k,v) in pairs(rounds_info))
 end
 
+function create_byzantine_validators_with_even_initial_stake(byzantine_validators)
+    return [Validator(i, ADMITION_STAKE, false, 0) for i in 1:byzantine_validators]
+end
+
+function create_honest_validators_with_even_initial_stake(honest_validators_count)
+    return [Validator(i, ADMITION_STAKE, true, 0) for i in 1:honest_validators_count]
+end
+
+function create_byzantine_validators_with_random_initial_stake(byzantine_validators)
+    return [Validator(i, sample(ADMITION_STAKE:50), false, 0) for i in 1:byzantine_validators]
+end
+
+function create_honest_validators_with_random_initial_stake(honest_validators_count)
+    return [Validator(i, sample(ADMITION_STAKE:50), true, 0) for i in 1:honest_validators_count]
+end
+
+function setup_simulation(validators_count::Int64, even_stake::Bool, honest_validators_proportion::Float64)   
+    @assert (0 <= honest_validators_proportion <= 1) "Honest validators proportion must be a value within [0, 1]"
+    honest_validators_count = round(validators_count * honest_validators_proportion)
+    byzantine_validators_count = validators_count - honest_validators_count
+    
+    return even_stake ?
+        cat(
+            create_honest_validators_with_even_initial_stake(honest_validators_count), 
+            create_honest_validators_with_even_initial_stake(byzantine_validators_count),
+            dims=1
+        ) :
+        cat(
+            create_honest_validators_with_random_initial_stake(honest_validators_count), 
+            create_byzantine_validators_with_random_initial_stake(byzantine_validators_count),
+            dims=1
+        )
+end
+
 begin
-    validators_pool = create_validators_pool_with(VALIDATORS_COUNT)
+    validators_pool = setup_simulation(VALIDATORS_COUNT, EVEN_INITIAL_STAKE, HONEST_NODE_PROPORTION)
     evolution_of_validators_stake_in_rounds = simulate_leader_election_n_rounds(ROUND_COUNT, validators_pool)
     df = DataFrame(evolution_of_validators_stake_in_rounds)
 
